2e00f6cdaaaa729f28ac7c6e9f1a482a0feee49e
Ziyuan Liu <lutepluto@gmail.com>
2014-11-11 17:30:48 +0800

    add basic sass task and default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,39 @@
-var gulp = require('gulp')，
-    sass = require('gulp-ruby-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    del = require('del');
 
-gulp.task('default', function() {
+/**
+ * ---------- sass task ----------
+ */
 
+gulp.task('sass', function() {
+  return gulp.src('scss/main.scss')
+    .pipe(sass({ style: 'expanded', sourcemap: true, sourcemapPath: '../scss/' }))
+    .on('error', function(err) { console.log(err.message); })
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(notify({ message: 'Sass task complete' }));
+});
+
+/**
+ * ---------- clean task ----------
+ */
+
+gulp.task('clean', function(cb) {
+  del(['dist/css'], cb);
+});
+
+/**
+ * ---------- default task ----------
+ */
+
+gulp.task('default', ['clean'], function() {
+  gulp.start('sass');
 });
