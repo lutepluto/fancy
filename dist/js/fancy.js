1a8b2abe2f2e0a1e6e4e7671457ad4a7cd3a1202
Ziyuan Liu <lutepluto@gmail.com>
2014-11-27 13:32:43 +0800

    add compiled fancy.js

---
+++ dist/js/fancy.js
@@ -0,0 +1,200 @@
+// actionsheet.js
+// ==================================================
+
++function($) {
+  'use strict'
+
+  var Actionsheet = function(element, options) {
+    this.$element = $(element)
+    this.$doc = $(document.body)
+    this.options = options
+    this.$backdrop =
+    this.isOpen = null
+
+    if(this.options.feedback) this.$element.on('click.select.fancy.actionsheet', '.actionsheet-feedback', $.proxy(this.select, this))
+  }
+
+  Actionsheet.prototype.toggle = function(_relatedTarget) {
+    return this.isOpen ? this.close() : this.open(_relatedTarget)
+  }
+
+  Actionsheet.prototype.open = function(_relatedTarget) {
+    var that = this
+    var e = $.Event('open.fancy.actionsheet', { relateTarget: _relatedTarget })
+    this.$element.trigger(e)
+
+    if(this.isOpen) return
+    this.isOpen = true
+
+    this.$element.one('click.dismiss.fancy.actionsheet', '[data-dismiss="actionsheet"]', $.proxy(this.close, this))
+
+    this.backdrop(function() {
+      var transition = $.support.transition && that.$element.hasClass('fade')
+      that.$element.show()
+
+      if(transition) that.$element[0].offsetWidth
+      that.$element.addClass('in')
+
+      var e = $.Event('opend.fancy.actionsheet')
+
+      transition ? 
+        that.$element.one($.support.transition.end, function() {
+          that.$element.trigger('focus').trigger(e)
+        }).emulateTransitionEnd(300) : that.$element.trigger('focus').trigger(e)
+    })
+  }
+
+  Actionsheet.prototype.close = function(e) {
+    if(e) e.preventDefault()
+    e = $.Event('close.fancy.actionsheet')
+    this.$element.trigger(e)
+
+    if(!this.isOpen) return
+    this.isOpen = false
+
+    this.$element.removeClass('in')
+
+    $.support.transition ?
+      this.$element
+        .one($.support.transition.end, $.proxy(this.hideActionsheet, this))
+        .emulateTransitionEnd(300) :
+      this.hideActionsheet()
+  }
+
+  Actionsheet.prototype.hideActionsheet = function() {
+    var that = this
+    this.$element.hide()
+    this.backdrop(function() {
+      that.$element.trigger('closed.fancy.actionsheet')
+    })
+  }
+
+  Actionsheet.prototype.backdrop = function(callback) {
+   var that = this
+   var animate = this.$element.hasClass('fade') ? 'fade' : ''
+
+   if(this.isOpen) {
+    var transition = $.support.transition
+    this.$backdrop = $('<div class="backdrop ' + animate + '"/>')
+      .appendTo(this.$doc)
+      .one('click.dismiss.fancy.actionsheet', $.proxy(this.close, this))
+
+    if(transition) this.$backdrop[0].offsetWidth
+
+    this.$backdrop.addClass('in')
+
+    if(!callback) return
+
+    transition ? this.$backdrop
+      .one($.support.transition.end, callback)
+      .emulateTransitionEnd(150) : callback()
+   } else if(!this.isOpen && this.$backdrop) {
+    this.$backdrop.removeClass('in')
+
+    var callbackRemove = function() {
+      that.removeBackdrop()
+      callback && callback()
+    }
+    $.support.transition && this.$backdrop.hasClass('fade') ?
+      this.$backdrop
+        .one($.support.transition.end, callbackRemove)
+        .emulateTransitionEnd(150) :
+      callbackRemove()
+
+   } else if(callback) {
+    callback()
+   }
+  }
+
+  Actionsheet.prototype.removeBackdrop = function() {
+    this.$backdrop && this.$backdrop.remove()
+    this.$backdrop = null
+  }
+
+  Actionsheet.prototype.select = function(evt) {
+    if(evt.target !== evt.currentTarget) return
+
+    var e = $.Event('select.fancy.actionsheet', { relateTarget: this.options.feedback })
+    this.$element.trigger(e)
+
+    var text = $(evt.target).text(),
+        value = $(evt.target).data('value')
+    $(this.options.feedback).val(text)
+    this.options.valueback && $(this.options.valueback).val(value)
+
+    this.$backdrop.trigger('click')
+    this.$element.trigger('selected.fancy.actionsheet')
+  }
+
+  var old = $.fn.actionsheet
+
+  function Plugin(option, _relatedTarget) {
+    return this.each(function() {
+      var $this = $(this),
+        data = $(this).data('fancy.actionsheet'),
+        options = $.extend({}, $(this).data(), typeof option == 'object' && option)
+
+      if(!data) $this.data('fancy.actionsheet', (data = new Actionsheet(this, options)))
+      if(typeof option == 'string') data[option](_relatedTarget)
+      else data.open(_relatedTarget)
+    })
+  }
+
+  $.fn.actionsheet = Plugin
+  $.fn.actionsheet.constructor = Actionsheet
+
+  $.fn.actionsheet.noConflict = function() {
+    $.fn.actionsheet = old
+    return this
+  }
+
+  $(document).on('click.fancy.actionsheet.data-api', '[data-toggle="actionsheet"]', function() {
+    var $this = $(this)
+    var $target = $($this.data('target'))
+    var options = $target.data('fancy.actionsheet') ? 'toggle' : $.extend({}, $target.data(), $this.data())
+
+    Plugin.call($target, options, this)
+  })
+
+}(window.Zepto || window.jQuery)
+
+
+// transition.js
+// http://www.modernizr.com/
+// Recommend reading http://blog.alexmaccaw.com/css-transitions
+// ==================================================
+
++function($) {
+  'use strict'
+
+  function transitionEnd() {
+    var el = document.createElement('fancy')
+
+    var transEndEventNames = {
+      'WebkitTransition' : 'webkitTransitionEnd',
+      'MozTransition'    : 'transitionend',
+      'OTransition'      : 'oTransitionEnd otransitionend',
+      'transition'       : 'transitionend'
+    }
+
+    for (var name in transEndEventNames) {
+      if (el.style[name] !== undefined) {
+        return { end: transEndEventNames[name] }
+      }
+    }
+  }
+
+  $.fn.emulateTransitionEnd = function(duration) {
+    var called = false, $el = this
+    $(this).one($.support.transition.end, function() { called = true })
+    var callback = function() { if (!called) $($el).trigger($.support.transition.end) }
+    setTimeout(callback, duration)
+    return this
+  }
+
+  $(function() {
+    $.support.transition = transitionEnd()
+  })
+
+}(window.Zepto || window.jQuery)
+
